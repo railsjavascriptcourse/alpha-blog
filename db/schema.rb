e7f4617900c6142d45d9c33a1ade7e05d270f565
Evgeny Rahman <evgeny.rahman@gmail.com>
2019-09-03 14:02:21 -0400

    Security updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120234529) do
+ActiveRecord::Schema.define(version: 2017_11_20_234529) do
 
   create_table "article_categories", force: :cascade do |t|
     t.integer "article_id"
