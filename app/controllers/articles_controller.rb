6fcd43f61cd57103ad0f676a3376e4524a60b08e
Evgeny Rahman <evgeny.rahman@gmail.com>
2020-06-02 16:54:41 -0400

    Fixing dependency vulnerabilities

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,19 +2,20 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
   before_action :require_user, except: [:index, :show]
   before_action :require_same_user, only: [:edit, :update, :destroy]
-  
+
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
   end
-  
+
   def new
     @article = Article.new
   end
-  
+
   def edit
   end
-  
+
   def create
+    debugger
     @article = Article.new(article_params)
     @article.user = current_user
     if @article.save
@@ -24,7 +25,7 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
-  
+
   def update
     if @article.update(article_params)
       flash[:success] = "Article was successfully updated"
@@ -33,22 +34,22 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def show
   end
-  
+
   def destroy
     @article.destroy
     flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
   end
-  
+
   private
-  
+
   def set_article
     @article = Article.find(params[:id])
   end
-  
+
   def article_params
     params.require(:article).permit(:title, :description, category_ids: [])
   end
