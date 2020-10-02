7e5e38dfdd8fb0388f463043b6cb9109bfba534a
宮田拓輔 <takuho.0806@gmail.com>
2020-10-03 08:52:31 +0900

    fix

--- app/javascript/components/Article.js
+++ app/javascript/components/Article.js
@@ -4,11 +4,15 @@ class Article extends React.Component {
   render () {
     return (
       <React.Fragment>
-      <div class="article-title">
+      <div className="article-title">
         <a href={this.props.path}>{this.props.title}</a>
       </div>
       <div className='article-body'>
         {this.props.description}
+        <div className="article-meta-details">
+          <small>Created by: {this.props.author}, {this.props.created_at} ago,
+                last updated: {this.props.updated_at} ago</small>
+        </div>
       </div>
       </React.Fragment>
     );
@@ -18,6 +22,9 @@ class Article extends React.Component {
 Article.propTypes = {
   title: PropTypes.string,
   path: PropTypes.string,
-  description: PropTypes.string
+  description: PropTypes.string,
+  author: PropTypes.string,
+  created_at: PropTypes.string,
+  updated_at: PropTypes.string,
 };
 export default Article
--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,8 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
   validates :user_id, presence: true
+
+  def author
+    user.username
+  end
 end
